2f0741727b870123275502bcd53926c20b1639c4
NIKKOR <52128703+NIKKOR@users.noreply.github.com>
2019-06-23 21:15:00 +0300

    Удалил отдельный файлы...

--- hard.py
+++
@@ -1,9 +0,0 @@
-a = float('inf')
-pow = a == a**2
-double = a == a*2
-big = a > 999999
-print('a = float("inf") ' + '\n' +
-      'a == a**2: ' + str(pow) + "\n" +
-      'a == a*2: ' + str(double) + "\n" +
-      'a > 999999: ' + str(big) + "\n" +
-      'That is ' + str(a) +  ' (infinity)')
--- swap.py
+++
@@ -1,6 +0,0 @@
-a = int(input('Enter first number\n'))
-b = int(input('Enter second number\n'))
-a = a, b,
-b = a[0]
-a = a[1]
-print('Now: a = ' + str(a) + ' and b = ' + str(b))
