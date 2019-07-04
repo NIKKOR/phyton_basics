2a9fb613f683976302b8830d28f29d4931a34d68
NIKKOR <korobckin.nickita@icloud.com>
2019-07-04 18:53:09 +0300

    Решил 2 задачу из дз4 (норм)

---
+++ lesson04/home_work/task2.py
@@ -0,0 +1,52 @@
+import re
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2='mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm' \
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV' \
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA' \
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV' \
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW' \
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC' \
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR' \
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm' \
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn' \
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS' \
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf' \
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH' \
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN' \
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ' \
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+
+print('Вам решение с re или без? (0 - с re, 1 - без)')
+re_check_2=bool(input())
+if re_check_2 == 1:
+    line_re_2=list(re.findall(r'[a-z]{2}[A-Z]+[A-Z]{2}', line_2))
+    line_re_2_res=[]
+    for el in line_re_2:
+        line_re_2_res.append(el[2: (len(el) - 2)])
+    print(line_re_2_res)
+elif re_check_2 == 1:
+    line_2_no_re=[]
+    lower_check=0
+    middle_check=0
+    middle_start = 0
+    upper_check=0
+    for i, el in enumerate(line_2):
+        if el.islower() and i > 0 and line_2[i - 1].islower():
+            lower_check=1
+        if lower_check == 1 and el.isupper():
+            middle_check=1
+            middle_start = i
+        if middle_check == 1 and el.islower and line_2[i-2:i-1].isupper:
+            upper_check=1
+        if upper_check == 1:
+            line_2_no_re.append(line_2[middle_start:i-2])
+    print(line_2_no_re)
